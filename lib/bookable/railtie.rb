e97efe905ecadf29d3a1ab1f462a0885c9868145
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-05 18:10:08 +0000

    moved generator and template folders

--- lib/bookable/railtie.rb
+++ lib/bookable/railtie.rb
@@ -4,10 +4,7 @@ module Bookable
 
   class Railtie <  Rails::Railtie
     generators do
-      require 'generators/resource_model.rb'
-      require 'generators/booking_model.rb'
-      require 'generators/bookable_controller.rb'
-      require 'generators/views_generator.rb'
+       
     end
   end
 
