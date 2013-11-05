d70a69414093b5977b6cfe9713b47ca28361ec68
Jonathan Tsang <chewymeister88@googlemail.com>
2013-11-05 21:07:53 +0000

    changed copy_file to template method in resource model generator

--- lib/generators/resource_model_generator.rb
+++ lib/generators/resource_model_generator.rb
@@ -3,11 +3,11 @@ class ResourceModelGenerator < Rails::Generators::Base
   argument :resource_name, :type => :string, :default => "resource"
 
   def generate_resource_model
-    copy_file "resource.rb", "app/models/#{resource_name}.rb"
+    template "resource.rb", "app/models/#{resource_name}.rb"
   end
 
   def create_resource_migration
-     generate "migration Create#{resource_name.camelize.pluralize} name:string"
+    generate "migration Create#{resource_name.camelize.pluralize} name:string"
   end
   
 end
